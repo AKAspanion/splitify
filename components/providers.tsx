0d372ba2d3e019015f3269d36198190e02b81bf8
Ankit Kumar Pandit <panditankit1995@gmail.com>
2024-02-09 18:25:59 +0530

    app progress bar added

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { AppProgressBar } from "next-nprogress-bar";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import { useTheme } from "next-themes";
@@ -12,6 +13,12 @@ export function Providers({ children }: { children: React.ReactNode }) {
         baseTheme: resolvedTheme === "dark" ? dark : undefined,
       }}
     >
+      <AppProgressBar
+        height="2px"
+        color="#355714"
+        options={{ showSpinner: false }}
+        shallowRouting
+      />
       {children}
     </ClerkProvider>
   );
