6f06843cb896233098de3b33c3fc7d1d0fe6f016
Ankit Kumar Pandit <panditankit1995@gmail.com>
2024-02-10 02:45:35 +0530

    PWA conversion done

--- config/site.ts
+++ config/site.ts
@@ -1,4 +1,53 @@
+const APP_NAME = "Splitify";
+const APP_DEFAULT_TITLE = "Splitify";
+const APP_TITLE_TEMPLATE = "%s - Splitify App";
+const APP_DESCRIPTION = "Simplify and manage splitsthe easy way!";
+
 export const siteConfig = {
-  name: "Splitify",
-  description: "Splitwise CLone",
+  name: APP_NAME,
+  description: APP_DESCRIPTION,
+  title: {
+    default: APP_NAME,
+    template: APP_TITLE_TEMPLATE,
+  },
+  icons: [
+    {
+      url: "/logo-512x512.webp",
+      href: "/logo-512x512.webp",
+    },
+  ],
+  metadata: {
+    applicationName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+    appleWebApp: {
+      capable: true,
+      statusBarStyle: "default",
+      title: APP_DEFAULT_TITLE,
+      // startUpImage: [],
+    },
+    formatDetection: {
+      telephone: false,
+    },
+    openGraph: {
+      type: "website",
+      siteName: APP_NAME,
+      title: {
+        default: APP_DEFAULT_TITLE,
+        template: APP_TITLE_TEMPLATE,
+      },
+      description: APP_DESCRIPTION,
+    },
+    twitter: {
+      card: "summary",
+      title: {
+        default: APP_DEFAULT_TITLE,
+        template: APP_TITLE_TEMPLATE,
+      },
+      description: APP_DESCRIPTION,
+    },
+  },
 };
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+import withSerwistInit from "@serwist/next";
+
+const withSerwist = withSerwistInit({
+  // Note: This is only an example. If you use Pages Router,
+  // use something else that works, such as "service-worker/index.ts".
+  swSrc: "app/sw.ts",
+  swDest: "public/sw.js",
+  // disable: process.env.NODE_ENV !== "production",
+});
+
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -21,4 +32,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withSerwist(nextConfig);
