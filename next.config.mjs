1234a8884f50b9ce7ce36f72e0096184e33844c2
Ankit Kumar Pandit <panditankit1995@gmail.com>
2024-02-10 17:57:33 +0530

    service worker disable fix

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ import withSerwistInit from "@serwist/next";
 const withSerwist = withSerwistInit({
   swSrc: "app/sw.ts",
   swDest: "public/sw.js",
-  disable: process.env.NODE_ENV !== "production",
+  disable: process.env.NODE_ENV === "develeopment",
 });
 
 const nextConfig = {
