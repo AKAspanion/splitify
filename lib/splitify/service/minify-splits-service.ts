0a851e7519774c1ed21eac744f809aac9ac4e034
Ankit Kumar Pandit <panditankit1995@gmail.com>
2024-03-05 23:45:13 +0530

    Minify calc bug fix

--- lib/splitify/service/minify-splits-service.ts
+++ lib/splitify/service/minify-splits-service.ts
@@ -58,10 +58,11 @@ export class MinifySplitsService {
 
     // Find the minimum of two amounts
     var min = fixedNum(this.minOf2(-amount[mxDebit], amount[mxCredit]));
-    amount[mxCredit] -= min;
-    amount[mxDebit] += min;
+    const newCredit = fixedNum(amount[mxCredit] - min);
+    const newDebit = fixedNum(amount[mxDebit] + min);
+    amount[mxCredit] = newCredit;
+    amount[mxDebit] = newDebit;
 
-    // this.balances.push(`Person ${mxDebit} pays ${min} to Person ${mxCredit}`);
     this.balances.push({
       user1Index: mxDebit,
       user2Index: mxCredit,
