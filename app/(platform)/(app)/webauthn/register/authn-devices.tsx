af3adf4d0d73f8ea6ab6ab4cc10689832d6e10fe
Ankit Kumar Pandit <panditankit1995@gmail.com>
2024-03-09 16:33:21 +0530

    Biometric page updated

--- app/(platform)/(app)/webauthn/register/authn-devices.tsx
+++ app/(platform)/(app)/webauthn/register/authn-devices.tsx
@@ -1,5 +1,7 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
+import { Alert, AlertTitle } from "@/components/ui/alert";
+import { Fingerprint } from "lucide-react";
 
 const AuthnDevices = async () => {
   const { userId } = auth();
@@ -7,11 +9,14 @@ const AuthnDevices = async () => {
     where: { userId: userId || "null" },
   });
 
-  const deviceText = devicesCount === 1 ? "device" : "devices";
+  const deviceText = devicesCount === 0 ? "device not" : "device";
 
   return (
     <div className="py-6">
-      {devicesCount} Authenticator {deviceText} found
+      <Alert>
+        <Fingerprint className="h-4 w-4" />
+        <AlertTitle> Authenticator {deviceText} found</AlertTitle>
+      </Alert>
     </div>
   );
 };
