48df41023cce7d42022d4e27b2e5bd44054c6ce2
Ankit Kumar Pandit <panditankit1995@gmail.com>
2024-03-25 02:42:57 +0530

    padding fix

--- app/(platform)/(app)/(screens)/groups/[id]/expenses-paginate.tsx
+++ app/(platform)/(app)/(screens)/groups/[id]/expenses-paginate.tsx
@@ -25,7 +25,7 @@ const ExpensesPaginate = ({ groupId }: { groupId: string }) => {
 
   return (
     <div className="pb-8 py-6">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 pb-6">
         {expenses?.map((e, i) => (
           <div key={e.id}>
             <ExpenseCard expense={e} key={e.id} />
